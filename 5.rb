c6d656435cb4e4e35930e6d8eafe503c6b27e7ab
Jeremy Stanley <jeremy@instructure.com>
2020-12-05 07:45:55 -0700

    duhhhh

--- 5.rb
+++ 5.rb
@@ -18,5 +18,9 @@ def parse_line(line)
   lb * 8 + lc
 end
 
-row_ids = STDIN.readlines.map { |line| parse_line(line).to_i }
+def parse_line_duhhhh(line)
+  line.tr('FLBR', '0011').to_i(2)
+end
+
+row_ids = STDIN.readlines.map { |line| parse_line_duhhhh(line).to_i }
 puts (row_ids.min..row_ids.max).to_a - row_ids
