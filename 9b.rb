08650d72a03d37c03627f57499ddd76c7918ea59
Jeremy Stanley <jeremy@instructure.com>
2020-12-09 08:07:35 -0700

    smrt

--- 9b.rb
+++ 9b.rb
@@ -8,3 +8,25 @@ for s in 2..1000
     break
   end
 end
+
+def smrt_sum(nums)
+  l = 0
+  h = 0
+  acc = nums[0]
+  while acc != TARGET
+    while acc < TARGET
+      h += 1
+      return nil if h == nums.size
+      acc += nums[h]
+    end
+    while acc > TARGET
+      acc -= nums[l]
+      l += 1
+      return nil if l == h
+    end
+  end
+  seq = nums[l..h]
+  seq.min + seq.max
+end
+
+puts smrt_sum(nums)
